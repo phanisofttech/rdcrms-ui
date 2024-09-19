baba8182857890e8eb654fa7a0bad5db5d262ec3
phanisofttech <p.phani10000@gmail.com>
2024-09-19 20:42:38 +0530

    Added files for layout design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { ReportsComponent } from './reports/reports.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: "", component: HomeComponent},
+ { path: "home", component: HomeComponent},
+ { path: "dashboard", component: DashboardComponent, 
+ children: [
+  { path: "", component: ReportsComponent},
+  { path: "reports", component: ReportsComponent }
+]},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/left-nav/left-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-nav',
+  templateUrl: './left-nav.component.html',
+  styleUrl: './left-nav.component.css'
+})
+export class LeftNavComponent {
+
+}
