e48666752dd2ced548e7ca48da81b810e1868486
Charan9701 <charanmungara2005@gmail.com>
2025-01-05 22:09:14 +0530

    url commented

--- src/app/all-users/service/all-users-service.service.ts
+++ src/app/all-users/service/all-users-service.service.ts
@@ -9,11 +9,11 @@ import { UserResponse } from '../../model/user-response';
 export class AllUsersServiceService {
 
   constructor(private http: HttpClient) { }
-  #userApi = "https://pst-user-service.df.r.appspot.com/api/user/all-users";
-  localUserApi = "http://localhost:5000/api/user/all-users"
+  //userApi = "https://pst-user-service.df.r.appspot.com/api/user/all-users";
+  userApi = "http://localhost:5000/api/user/all-users"
 
   getAllUsers(): Observable<UserResponse[]> {
 
-    return this.http.get<UserResponse[]>(this.localUserApi);
+    return this.http.get<UserResponse[]>(this.userApi);
   }
 }
