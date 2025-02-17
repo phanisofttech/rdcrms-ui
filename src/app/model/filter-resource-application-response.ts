859b37de42fd180973e32c99350c6fc6bffec717
Charan9701 <charanmungara2005@gmail.com>
2025-02-17 21:43:17 +0530

    reports page ui module added

---
+++ src/app/model/filter-resource-application-response.ts
@@ -0,0 +1,20 @@
+export interface FilterResourceApplicationResponse{
+
+    country : any ;
+
+    state : any ;
+
+    district : any ;
+
+    mandal : any ;
+
+    village : any ;
+
+    applicationsRecevied : any ;
+
+    applicationsInprogress : any ;
+
+    applicationsApproved : any ;
+
+    applicationsRejected : any ;
+}
